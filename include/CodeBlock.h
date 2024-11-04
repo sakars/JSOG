a63f489be5c726daa8994dcd9f3730e0a3004143
Sakars <o.putaans@gmail.com>
2024-11-04 17:43:08 +0000

    added tests and custom indents to code blocks

--- include/CodeBlock.h
+++ include/CodeBlock.h
@@ -1,5 +1,5 @@
 #include <string>
-#include <strstream>
+#include <sstream>
 #include <vector>
 #include <variant>
 #include <stack>
@@ -7,9 +7,21 @@
 class CodeBlock
 {
   std::vector<std::variant<std::string, CodeBlock>> lines;
+  std::reference_wrapper<CodeBlock> parent = *this;
+  struct _Inc_Ty
+  {
+  };
+  struct _Dec_ty
+  {
+  };
 
 public:
-  CodeBlock() = default;
+  static constexpr _Inc_Ty inc = _Inc_Ty();
+  static constexpr _Dec_ty dec = _Dec_ty();
+
+  std::string indent;
+
+  CodeBlock(std::string default_indent = "  ") : indent(default_indent) {};
 
   CodeBlock &operator<<(const std::string &line)
   {
@@ -23,39 +35,83 @@ public:
     return *this;
   }
 
+  CodeBlock &operator<<(const _Inc_Ty &)
+  {
+    CodeBlock block(indent);
+    block.parent = *this;
+    lines.push_back(block);
+    return std::get<CodeBlock>(lines.back());
+  }
+
+  CodeBlock &operator<<(const _Dec_ty &)
+  {
+    return parent;
+  }
+
   std::string str() const
   {
-    struct CodeBlockProgress
+    std::ostringstream out;
+    for (const auto &line : lines)
     {
-      std::vector<std::variant<std::string, CodeBlock>>::const_iterator it;
-      std::vector<std::variant<std::string, CodeBlock>>::const_iterator end;
-    };
-    std::stack<CodeBlockProgress> blockStack;
-    blockStack.emplace(lines.cbegin(), lines.cend());
-    std::ostrstream out;
-    do
-    {
-      auto &block = blockStack.top();
-      blockStack.pop();
-      for (; block.it != block.end; block.it++)
+      if (std::holds_alternative<std::string>(line))
       {
-        if (std::holds_alternative<std::string>(*block.it))
+        out << std::get<std::string>(line) << std::endl;
+      }
+      else if (std::holds_alternative<CodeBlock>(line))
+      {
+        std::istringstream stream(std::get<CodeBlock>(line).str());
+        std::string line;
+        while (std::getline(stream, line, '\n'))
         {
-          const auto indent = blockStack.size();
-          for (size_t i = 0; i < indent; i++)
-          {
-            out << "  ";
-          }
-          out << std::get<std::string>(*block.it) << std::endl;
-        }
-        else
-        {
-          blockStack.emplace(block.it + 1, block.end);
-          blockStack.emplace(std::get<CodeBlock>(*block.it).lines.cbegin(), std::get<CodeBlock>(*block.it).lines.cend());
-          break;
+          out << indent << line << '\n';
         }
       }
-
-    } while (!blockStack.empty());
+      else
+      {
+        throw std::runtime_error("Unknown type in lines");
+      }
+    }
+    return out.str();
   }
+
+  // std::string str() const
+  // {
+  //   struct CodeBlockProgress
+  //   {
+  //     std::vector<std::variant<std::string, CodeBlock>>::const_iterator it;
+  //     std::vector<std::variant<std::string, CodeBlock>>::const_iterator end;
+  //   };
+  //   std::stack<CodeBlockProgress> blockStack;
+  //   blockStack.emplace(lines.cbegin(), lines.cend());
+  //   std::ostringstream out;
+  //   do
+  //   {
+  //     auto &block = blockStack.top();
+  //     blockStack.pop();
+  //     for (; block.it != block.end; block.it++)
+  //     {
+  //       if (std::holds_alternative<std::string>(*block.it))
+  //       {
+  //         const auto indent = blockStack.size();
+  //         for (size_t i = 0; i < indent; i++)
+  //         {
+  //           out << "  ";
+  //         }
+  //         out << std::get<std::string>(*block.it) << std::endl;
+  //       }
+  //       else if (std::holds_alternative<CodeBlock>(*block.it))
+  //       {
+  //         blockStack.emplace(block.it + 1, block.end);
+  //         blockStack.emplace(std::get<CodeBlock>(*block.it).lines.cbegin(), std::get<CodeBlock>(*block.it).lines.cend());
+  //         break;
+  //       }
+  //       else
+  //       {
+  //         throw std::runtime_error("Unknown type in lines");
+  //       }
+  //     }
+
+  //   } while (!blockStack.empty());
+  //   return out.str();
+  // }
 };

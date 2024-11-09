319646c7e45f173ca0f6cc20c5c148f028e0d7db
Sakars <o.putaans@gmail.com>
2024-11-09 15:01:27 +0000

    Excluded reserved keywords from sanitized strings

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -1,6 +1,7 @@
 #include "StringUtils.h"
 #include <stdexcept>
 #include <unordered_map>
+#include <unordered_set>
 
 std::string sanitizeString(std::string s) {
   static const std::string hex = "0123456789ABCDEF";
@@ -26,6 +27,113 @@ std::string sanitizeString(std::string s) {
       outStr += escapeChar(static_cast<unsigned char>(c));
     }
   }
+
+  // If the string is empty, add a placeholder.
+  if (outStr.empty()) {
+    outStr = "_";
+  }
+  // If the string is a C++ keyword, add a placeholder.
+  static const std::unordered_set<std::string> cpp_keywords = {
+      "alignas",
+      "alignof",
+      "and",
+      "and_eq",
+      "asm",
+      "atomic_cancel",
+      "atomic_commit",
+      "atomic_noexcept",
+      "auto",
+      "bitand",
+      "bitor",
+      "bool",
+      "break",
+      "case",
+      "catch",
+      "char",
+      "char8_t",
+      "char16_t",
+      "char32_t",
+      "class",
+      "compl",
+      "concept",
+      "const",
+      "consteval",
+      "constexpr",
+      "constinit",
+      "const_cast",
+      "continue",
+      "co_await",
+      "co_return",
+      "co_yield",
+      "decltype",
+      "default",
+      "delete",
+      "do",
+      "double",
+      "dynamic_cast",
+      "else",
+      "enum",
+      "explicit",
+      "export",
+      "extern",
+      "false",
+      "float",
+      "for",
+      "friend",
+      "goto",
+      "if",
+      "inline",
+      "int",
+      "long",
+      "mutable",
+      "namespace",
+      "new",
+      "noexcept",
+      "not",
+      "not_eq",
+      "nullptr",
+      "operator",
+      "or",
+      "or_eq",
+      "private",
+      "protected",
+      "public",
+      "register",
+      "reinterpret_cast",
+      "requires",
+      "return",
+      "short",
+      "signed",
+      "sizeof",
+      "static",
+      "static_assert",
+      "static_cast",
+      "struct",
+      "switch",
+      "synchronized",
+      "template",
+      "this",
+      "thread_local",
+      "throw",
+      "true",
+      "try",
+      "typedef",
+      "typeid",
+      "typename",
+      "union",
+      "unsigned",
+      "using",
+      "virtual",
+      "void",
+      "volatile",
+      "wchar_t",
+      "while",
+      "xor",
+      "xor_eq"};
+  if (cpp_keywords.contains(outStr)) {
+    outStr += "_";
+  }
+
   return outStr;
 }
 

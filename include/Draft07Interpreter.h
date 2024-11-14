3bb77bec40660807706b07460d06a5b3441e23aa
Sakars <o.putaans@gmail.com>
2024-11-14 15:44:13 +0000

    Added include guard to draft07 interpreter

--- include/Draft07Interpreter.h
+++ include/Draft07Interpreter.h
@@ -1,7 +1,10 @@
-
+#ifndef DRAFT07INTERPRETER_H
+#define DRAFT07INTERPRETER_H
 
 #include "IdentifiableSchema.h"
 #include "IndexedSyncedSchema.h"
 
 IndexedSyncedSchema
 interpretDraft07IdentifiableSchema(const IdentifiableSchema&);
+
+#endif // DRAFT07INTERPRETER_H

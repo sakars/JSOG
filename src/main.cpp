448a70e295bb4a21ee5c1770d07efdbadce8c515
Sakars <o.putaans@gmail.com>
2024-11-15 20:52:17 +0000

    Reworked main to have an option to output a single .cpp file to optimize compile times

--- src/main.cpp
+++ src/main.cpp
@@ -68,119 +68,27 @@ int main() {
 
   SyncedSchema::dumpSchemas(syncedSchemas);
 
-  for (auto& schema : syncedSchemas) {
-    std::filesystem::create_directory("../locals/schemas");
-    std::ofstream header("../locals/schemas/" + schema->getHeaderFileName());
-    header << schema->generateDeclaration().str();
-    header.close();
-    std::ofstream source("../locals/schemas/" + schema->getSourceFileName());
-    source << schema->generateDefinition().str();
+  const bool combineSourceFiles = true;
+
+  if (combineSourceFiles) {
+    std::ofstream source("../locals/schemas/schemas.cpp");
+    for (auto& schema : syncedSchemas) {
+      std::filesystem::create_directory("../locals/schemas");
+      std::ofstream header("../locals/schemas/" + schema->getHeaderFileName());
+      header << schema->generateDeclaration().str();
+      header.close();
+      source << schema->generateDefinition().str();
+    }
     source.close();
+  } else {
+    for (auto& schema : syncedSchemas) {
+      std::filesystem::create_directory("../locals/schemas");
+      std::ofstream header("../locals/schemas/" + schema->getHeaderFileName());
+      header << schema->generateDeclaration().str();
+      header.close();
+      std::ofstream source("../locals/schemas/" + schema->getSourceFileName());
+      source << schema->generateDefinition().str();
+      source.close();
+    }
   }
-
-  //   SyncedSchema subschemaS("Subs");
-  //   SyncedSchema subschema1("Sub1");
-  //   subschemaS.ref_ = std::ref(subschema1);
-  //   subschema1.properties_.emplace("a", std::ref(subschemaS));
-  //   subschema1.properties_.emplace("b", std::ref(subschemaS));
-  //   subschema1.required_ = {"a"};
-  //   subschema1.minProperties_ = 1;
-  //   subschema1.maxProperties_ = 4;
-  //   subschema1.default_ = "{\"a\": true}"_json;
-  //   SyncedSchema subschema2("Sub2");
-  //   subschema2.type_ = {IndexedSyncedSchema::Type::Boolean};
-  //   SyncedSchema subschema3("Sub3");
-  //   // subschema3.type_ = {SyncedSchema::Type::Number};
-  //   SyncedSchema schema("Test");
-
-  //   schema.type_ = std::set<IndexedSyncedSchema::Type>();
-  //   schema.type_->insert(IndexedSyncedSchema::Type::Object);
-  //   schema.type_->insert(IndexedSyncedSchema::Type::Array);
-  //   schema.type_->insert(IndexedSyncedSchema::Type::Integer);
-  //   schema.tupleableItems_ = {std::ref(subschema1), std::ref(subschema2)};
-  //   schema.items_ = std::ref(subschema3);
-  //   schema.properties_.emplace("test", std::ref(subschemaS));
-  //   schema.const_ = R"(
-  // {
-  //   "test": "test",
-  //   "test2": "test2",
-  //   "test3": [1, 2, 3],
-  //   "test4": {
-  //     "test5": "test5"
-  //   }
-  // })"_json;
-
-  //   std::ofstream header(schema.getHeaderFileName());
-  //   header << schema.generateDeclaration().str();
-  //   header.close();
-  //   std::ofstream source(schema.getSourceFileName());
-  //   source << schema.generateDefinition().str();
-  //   source.close();
-  //   std::ofstream sub1header(subschema1.getHeaderFileName());
-  //   sub1header << subschema1.generateDeclaration().str();
-  //   sub1header.close();
-  //   std::ofstream sub1source(subschema1.getSourceFileName());
-  //   sub1source << subschema1.generateDefinition().str();
-  //   sub1source.close();
-  //   std::ofstream sub2header(subschema2.getHeaderFileName());
-  //   sub2header << subschema2.generateDeclaration().str();
-  //   sub2header.close();
-  //   std::ofstream sub2source(subschema2.getSourceFileName());
-  //   sub2source << subschema2.generateDefinition().str();
-  //   sub2source.close();
-  //   std::ofstream sub3header(subschema3.getHeaderFileName());
-  //   sub3header << subschema3.generateDeclaration().str();
-  //   sub3header.close();
-  //   std::ofstream sub3source(subschema3.getSourceFileName());
-  //   sub3source << subschema3.generateDefinition().str();
-  //   sub3source.close();
-  //   std::ofstream subsheader(subschemaS.getHeaderFileName());
-  //   subsheader << subschemaS.generateDeclaration().str();
-  //   subsheader.close();
-  //   std::ofstream subssource(subschemaS.getSourceFileName());
-  //   subssource << subschemaS.generateDefinition().str();
-  //   subssource.close();
-  //   const SyncedSchema& true_ = SyncedSchema::getTrueSchema();
-  //   std::ofstream trueheader(true_.getHeaderFileName());
-  //   trueheader << true_.generateDeclaration().str();
-  //   trueheader.close();
-  //   std::ofstream truesource(true_.getSourceFileName());
-  //   truesource << true_.generateDefinition().str();
-  //   truesource.close();
-
-  //   nlohmann::json schema = R"(
-  // {
-  //     "$schema": "http://json-schema.org/draft-07/schema#",
-  //     "$id": "http://json-schema.org/draft-07/schema#",
-  //     "title": "Core schema meta-schema",
-  //     "properties": {
-  //       "test": {
-  //         "#ref": "#"
-  //       }
-  //     }
-  // })"_json;
-
-  // ResourceIndex index;
-  // index.addResource(schema, {"file:///root.json"}, "file:///root.json");
-  // for (auto &[uri, resource] : index)
-  // {
-  //   std::cout << uri << ":" << std::endl
-  //             << resource->json.dump(2) << std::endl;
-  // }
-  // index.markForBuild("file:///root.json");
-  // index.build();
-  // index.generateUniqueSchemaNames();
-  // index.resolveReferences();
-  // std::ofstream out("myfile.cpp");
-
-  // out << index.generateDefinition(**index["file:///myfile"]);
-  // std::cout << index.generateDefinition(**index["file:///myfile"]) <<
-  // std::endl; out.close();
-  // // std::cout << (*index["file:///myfile"])->schema->generateDefinition()
-  // //           << std::endl;
-
-  // std::cout << (*index["file:///myfile"])->schema->getTypeName() <<
-  // std::endl;
-
-  // index.generateResources("./outDir");
 }

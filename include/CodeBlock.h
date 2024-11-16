52562496ca62b7a85be736956421dc1ddb3d2033
Sakars <o.putaans@gmail.com>
2024-11-16 19:10:35 +0000

    CodeBlock.h cleanup

--- include/CodeBlock.h
+++ include/CodeBlock.h
@@ -78,47 +78,6 @@ public:
     }
     return out.str();
   }
-
-  // std::string str() const
-  // {
-  //   struct CodeBlockProgress
-  //   {
-  //     std::vector<std::variant<std::string, CodeBlock>>::const_iterator it;
-  //     std::vector<std::variant<std::string, CodeBlock>>::const_iterator end;
-  //   };
-  //   std::stack<CodeBlockProgress> blockStack;
-  //   blockStack.emplace(lines.cbegin(), lines.cend());
-  //   std::ostringstream out;
-  //   do
-  //   {
-  //     auto &block = blockStack.top();
-  //     blockStack.pop();
-  //     for (; block.it != block.end; block.it++)
-  //     {
-  //       if (std::holds_alternative<std::string>(*block.it))
-  //       {
-  //         const auto indent = blockStack.size();
-  //         for (size_t i = 0; i < indent; i++)
-  //         {
-  //           out << "  ";
-  //         }
-  //         out << std::get<std::string>(*block.it) << std::endl;
-  //       }
-  //       else if (std::holds_alternative<CodeBlock>(*block.it))
-  //       {
-  //         blockStack.emplace(block.it + 1, block.end);
-  //         blockStack.emplace(std::get<CodeBlock>(*block.it).lines.cbegin(),
-  //         std::get<CodeBlock>(*block.it).lines.cend()); break;
-  //       }
-  //       else
-  //       {
-  //         throw std::runtime_error("Unknown type in lines");
-  //       }
-  //     }
-
-  //   } while (!blockStack.empty());
-  //   return out.str();
-  // }
 };
 struct Indent {
   CodeBlock& block;

07612ddfac3a4f56f9ecb288c22e338e670906b1
Sakars <o.putaans@gmail.com>
2024-11-24 18:24:34 +0000

    some variant output static tests

---
+++ tests/output_tests/test_stringNumberVariant.cpp
@@ -0,0 +1,35 @@
+
+#define CATCH_CONFIG_RUNTIME_STATIC_REQUIRE
+#include <catch2/catch_all.hpp>
+
+#include "StringNumberVariant.h"
+
+TEST_CASE("Static StringNumberVariant checks") {
+  STATIC_CHECK((std::is_same_v<JSOG::StringNumberVariant::StringNumberVariant,
+                               std::variant<std::string, double>> ||
+                std::is_same_v<JSOG::StringNumberVariant::StringNumberVariant,
+                               std::variant<double, std::string>>));
+
+  using ConstructType = decltype(JSOG::StringNumberVariant::construct);
+  STATIC_REQUIRE(std::is_same_v<
+                 ConstructType,
+                 std::optional<JSOG::StringNumberVariant::StringNumberVariant>(
+                     const nlohmann::json&)>);
+
+  using RawExportType = decltype(JSOG::StringNumberVariant::rawExport);
+  STATIC_REQUIRE(std::is_same_v<
+                 RawExportType,
+                 nlohmann::json(
+                     const JSOG::StringNumberVariant::StringNumberVariant&)>);
+
+  using JsonType = decltype(JSOG::StringNumberVariant::json);
+  STATIC_REQUIRE(std::is_same_v<
+                 JsonType,
+                 std::optional<nlohmann::json>(
+                     const JSOG::StringNumberVariant::StringNumberVariant&)>);
+
+  using ValidateType = decltype(JSOG::StringNumberVariant::validate);
+  STATIC_REQUIRE(std::is_same_v<
+                 ValidateType,
+                 bool(const JSOG::StringNumberVariant::StringNumberVariant&)>);
+}
